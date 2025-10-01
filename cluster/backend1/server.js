dfa70d6617af00f0fc29b47a3ca685f60cd2ba35
shivani <shivaniw69@gmail.com>
2025-10-01 18:25:01 +0530

    server

--- cluster/backend1/server.js
+++ cluster/backend1/server.js
@@ -61,12 +61,39 @@
 //====================================================PID CLUSTER=========================================================================
 
 
+// const cluster = require('cluster');
+// const http = require('http');
+// const os = require('os');
+
+// if (cluster.isMaster) {
+//   // Get the number of CPU cores available
+//   const numCPUs = os.cpus().length;
+//   console.log(`Master process PID: ${process.pid}`);
+//   console.log(`Forking ${numCPUs} workers...`);
+//   // Fork workers
+//   for (let i = 0; i < numCPUs; i++) {
+//     cluster.fork();
+//   }
+
+//   // Listen for worker exit events
+//   cluster.on('exit', (worker, code, signal) => {
+//     console.log(`Worker ${worker.process.pid} died. Forking a new worker...`);
+//     cluster.fork();
+//   });
+// } else {
+//   // Worker processes handle incoming HTTP requests
+//   http.createServer((req, res) => {
+//     res.writeHead(200);
+//     res.end(`Handled by worker process PID: ${process.pid}\n`);
+//   }).listen(3000);
+//   console.log(`Worker process PID: ${process.pid} is running`);
+// }
+
 const cluster = require('cluster');
-const http = require('http');
 const os = require('os');
+const express = require('express');
 
 if (cluster.isMaster) {
-  // Get the number of CPU cores available
   const numCPUs = os.cpus().length;
   console.log(`Master process PID: ${process.pid}`);
   console.log(`Forking ${numCPUs} workers...`);
@@ -74,17 +101,16 @@ if (cluster.isMaster) {
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
-
-  // Listen for worker exit events
   cluster.on('exit', (worker, code, signal) => {
     console.log(`Worker ${worker.process.pid} died. Forking a new worker...`);
     cluster.fork();
   });
 } else {
-  // Worker processes handle incoming HTTP requests
-  http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end(`Handled by worker process PID: ${process.pid}\n`);
-  }).listen(3000);
-  console.log(`Worker process PID: ${process.pid} is running`);
+  const app = express();
+  app.get('/', (req, res) => {
+    res.send(`Hello from worker process PID: ${process.pid}`);
+  });
+  app.listen(3000, () => {
+    console.log(`Worker process PID: ${process.pid} is running`);
+  });
 }
