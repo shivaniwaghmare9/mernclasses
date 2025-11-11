b886f87d0178b0d703ec9055fbd07c8f106d11fa
shivani <shivaniw69@gmail.com>
2025-11-11 22:03:27 +0530

    route

--- cookies/backend/routes/cookiesRoutes.js
+++ cookies/backend/routes/cookiesRoutes.js
@@ -1,3 +1,13 @@
+// const express = require("express");
+// const route = express.Router();
+// const cookieController=require("../controllers/cookieController")
+
+// route.post("/register",cookieController.Register);
+// route.post("/login",cookieController.Login);
+// route.get("/profile",cookieController.Profile);
+// route.post("/logout",cookieController.Logout);
+
+// module.exports = route;
 const express = require("express");
 const route = express.Router();
 const cookieController=require("../controllers/cookieController")
