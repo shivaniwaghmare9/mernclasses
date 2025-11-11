003a306147a2858daf5f8c9718cff13ecea7365c
shivani <shivaniw69@gmail.com>
2025-11-11 22:05:18 +0530

    js

--- cookies/backend/models/User.js
+++ cookies/backend/models/User.js
@@ -1,3 +1,11 @@
+// const mongoose = require("mongoose");
+
+// const userSchema = new mongoose.Schema({
+//   username: { type: String, required: true, unique: true },
+//   password: { type: String, required: true }
+// });
+
+// module.exports = mongoose.model("User", userSchema);
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
