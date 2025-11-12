1503c40aa49bfbe2adface7641e2ee0f019b0264
shivani <shivaniw69@gmail.com>
2025-11-12 22:51:41 +0530

    model

--- nodemailer/server/models/mailerModel.js
+++ nodemailer/server/models/mailerModel.js
@@ -1,12 +1,4 @@
 
-// const mongoose=require("mongoose");
-// const mailerSchema=new mongoose.Schema({
-//     email:String,
-//     name:String,
-//     comment:String
-// })
-// module.exports=mongoose.model("mailer",mailerSchema)
-
 const mongoose=require("mongoose");
 const mailerSchema=new mongoose.Schema({
     email:String,
