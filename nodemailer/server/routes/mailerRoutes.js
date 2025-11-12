c48d2d938a72468600e7cdbc5a5c4c08748efff2
shivani <shivaniw69@gmail.com>
2025-11-12 22:50:56 +0530

    route

--- nodemailer/server/routes/mailerRoutes.js
+++ nodemailer/server/routes/mailerRoutes.js
@@ -1,12 +1,4 @@
 
-// const express=require("express");
-// const route=express.Router();
-// const mailerController=require("../controllers/mailerController")
-
-// route.post("/emailsend",mailerController.emailSend)
-
-// module.exports=route;
-
 const express=require("express");
 const route=express.Router();
 const mailerController=require("../controllers/mailerController")
