ec96937269d575d31fffa8ac8f994aa3b78a9644
shivani <shivaniw69@gmail.com>
2025-11-14 22:00:04 +0530

    control

--- sessions/backend/controllers/sessionController.js
+++ sessions/backend/controllers/sessionController.js
@@ -1,17 +1,3 @@
-// // controllers/sessionController.js
-// const Home = (req, res) => {
-//   req.session.myname = "shivani";
-//   res.send("okk");
-// };
-
-// const Display = (req, res) => {
-//   const myname = req.session.myname;
-//   res.send(myname || "No session found!");
-// };
-// module.exports={
-//     Home,
-//     Display
-// }
 // controllers/sessionController.js
 const Home = (req, res) => {
   req.session.myname = "shivani";
