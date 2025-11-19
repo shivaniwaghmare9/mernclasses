f6677d6d08b109732bfd5579222f03edbef9a5d4
shivani <shivaniw69@gmail.com>
2025-11-20 04:06:20 +0530

    route

--- sessions/backend/routes/sessionRoutes.js
+++ sessions/backend/routes/sessionRoutes.js
@@ -1,13 +1,3 @@
-// // routes/testRoutes.js
-// const express = require("express");
-// const router = express.Router();
-// const sessionController= require("../controllers/sessionController");
-
-// // Routes
-// router.get("/", sessionController.Home);
-// router.get("/display", sessionController.Display);
-
-// module.exports = router;
 // routes/testRoutes.js
 const express = require("express");
 const router = express.Router();
