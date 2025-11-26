289aa6f17f028cdf22d35cc7287ff999775e89a2
shivani <shivaniw69@gmail.com>
2025-11-26 22:03:01 +0530

    route

--- Mernfinalproject/server/models/doctorModel.js
+++ Mernfinalproject/server/models/doctorModel.js
@@ -1,3 +1,44 @@
+// const mongoose = require('mongoose')
+
+// const doctorSchema = new  mongoose.Schema(
+//     {
+//         doctorname: {
+//             type: String,
+//             required: true,
+//         }, 
+//         speciality: {
+//             type: String,
+//             required: true,
+//         },
+//          city: {
+//             type: String,
+//             required: true,
+//         },
+//          address: {
+//             type: String,
+//             required: true,
+//         },
+//          image: {
+//             type: String,
+//             required: true,
+//         },
+//          contact: {
+//             type: String,
+//             required: true,
+//         },
+//          email: {
+//             type: String,
+//             required: true,
+//         },
+//          password: {
+//             type: String,
+//             required: true,
+//         }
+        
+//     }, {timestamps: true}
+// )
+
+// module.exports = mongoose.model('doctor', doctorSchema)
 const mongoose = require('mongoose')
 
 const doctorSchema = new  mongoose.Schema(
