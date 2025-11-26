558fd4c496fb1c8b72c8a808114cd537d457e148
shivani <shivaniw69@gmail.com>
2025-11-26 21:58:10 +0530

    app

--- Mernfinalproject/client/src/App.jsx
+++ Mernfinalproject/client/src/App.jsx
@@ -1,3 +1,22 @@
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Layout from "./Layout";
+
+
+// const App=()=>{
+//   return(
+//     <>
+//      <BrowserRouter>
+//          <Routes>
+//           <Route path="/" element={<Layout/>}>
+          
+          
+//           </Route>
+//          </Routes>
+//      </BrowserRouter>
+//     </>
+//   )
+// }
+// export default App;
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 
