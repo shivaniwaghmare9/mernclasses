69d980910f5c610c9a5e51114ef38969a33701ee
shivani <shivaniw69@gmail.com>
2025-11-26 22:01:15 +0530

    controler

--- Mernfinalproject/server/routes/doctorRoutes.js
+++ Mernfinalproject/server/routes/doctorRoutes.js
@@ -1,3 +1,15 @@
+// const express = require("express");
+// const route = express.Router();
+// const doctorController= require("../controllers/doctorController");
+// const uploadMiddleware = require("../middleware/uploadMiddleware");
+
+
+// route.post("/doctorsave", uploadMiddleware.upload.single("file"), doctorController.doctorSave);
+
+
+// route.post("/doctorlogin", doctorController.doctorLogin);
+
+// module.exports= route;
 const express = require("express");
 const route = express.Router();
 const doctorController= require("../controllers/doctorController");
